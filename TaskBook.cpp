614921a09d33c51da61fca393ab03b588921fdf8
vladick <bravilmaksim@gmail.com>
2026-05-05 23:56:37 +0300

    Started new project

---
+++ TaskBook.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread> // ??? std::this_thread::sleep_for
+#include <chrono> // ??? std::chrono::seconds
+#include <string>
+#include <cstdlib> // ??? std::srand, std::rand
+using namespace std;
+
+
+
+
+
+int main()
+{
+this_thread::sleep_for(chrono::seconds(2));  
+
+return 0;
+}
+
+
+
+
+
